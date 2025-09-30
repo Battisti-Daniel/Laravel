c9152ebc30f40f611a9d50d0afb23274cc811d52
Battisti-Daniel <danielbattisti@outlook.com.br>
2025-09-30 14:21:12 -0300

    First commit

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -5,14 +5,11 @@
 
 
 
-<
-{{$type}}
+<{{$type}}
     aria-current="{{ $active ? 'page' : false}}"
     class=" {{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white'}} rounded-md  px-3 py-2 text-sm font-medium"
     {{$attributes}}
 >
 
     {{ $slot }}
-</
-{{$type}}
->
+</{{$type}}>
