f92b13a9757954a002cd5c1b6eb9d2a0992523a3
Battisti-Daniel <danielbattisti@outlook.com.br>
2025-09-30 14:13:37 -0300

    First commit

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
+    <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="h-full">
+
+    <div class="min-h-full">
+        <nav class="bg-gray-800">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="flex h-16 items-center justify-between">
+                    <div class="flex items-center">
+                        <div class="shrink-0">
+                            <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="size-8" />
+                        </div>
+                        <div class="hidden md:block">
+                            <div class="ml-10 flex items-baseline space-x-4">
+                                <x-nav-link href="/" :active="request()->is('/')" type="button">Home</x-nav-link>
+                                <x-nav-link href="/about" :active="request()->is('about')" type="a">About</x-nav-link>
+                                <x-nav-link href="/contact" :active="request()->is('contact')" type="a">Contact</x-nav-link>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hidden md:block">
+                        <div class="ml-4 flex items-center md:ml-6">
+                            <button type="button" class="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                                <span class="absolute -inset-1.5"></span>
+                                <span class="sr-only">View notifications</span>
+                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+                                    <path d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </nav>
+
+
+        <header class="relative bg-white shadow-sm">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
+
+
+    </body>
+</html>
---
+++ resources/views/Components/nav-link.blade.php
@@ -0,0 +1,18 @@
+@props([
+    'active' => false,
+    'type' => 'a'
+    ])
+
+
+
+<
+{{$type}}
+    aria-current="{{ $active ? 'page' : false}}"
+    class=" {{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white'}} rounded-md  px-3 py-2 text-sm font-medium"
+    {{$attributes}}
+>
+
+    {{ $slot }}
+</
+{{$type}}
+>
