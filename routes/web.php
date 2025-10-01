0faf985233343436d954f3882e052de1caac8ce9
Battisti-Daniel <danielbattisti@outlook.com.br>
2025-10-01 13:45:25 -0300

    First commit

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs',[
+        'jobs' => Job::all()
+    ]);
 });
 
 Route::get('/contact', function (){
     return view('contact');
+});
+
+Route::get('/job/{id}', function ($id){
+
+    $job = Job::find($id);
+
+    return view('job', [
+
+        'job' => $job,
+    ]);
 });
